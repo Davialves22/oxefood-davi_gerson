86599d66e9a9fb58dd670a649c96eade544829fc
Davi <dgas1@discente.ifpe.edu.br>
2025-04-23 20:39:02 -0300

    adicionado funçoes de pesquisar todos e pesquisar por id

--- src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
+++ src/main/java/br/com/ifpe/oxefood/api/cliente/ClienteController.java
@@ -1,9 +1,13 @@
 package br.com.ifpe.oxefood.api.cliente;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,11 +24,22 @@ public class ClienteController {
   @Autowired
   private ClienteService clienteService;
 
+  // função salvar
   @PostMapping
   public ResponseEntity<Cliente> save(@RequestBody ClienteRequest request) {
 
     Cliente cliente = clienteService.save(request.build());
     return new ResponseEntity<Cliente>(cliente, HttpStatus.CREATED);
+  }
+
+  @GetMapping
+  public List<Cliente> listarTodos() {
+    return clienteService.listarTodos();
+  }
+
+  @GetMapping("/{id}")
+  public Cliente obterPorID(@PathVariable Long id) {
+    return clienteService.obterPorID(id);
   }
 
 }
